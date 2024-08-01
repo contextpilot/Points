b63ee99fefe3bd785c1f3425a4be3e4b90dfb8e6
Pilot Context <pilot.context@cryptitalk.com>
2024-08-01 18:58:53 +0000

    make chat widget work

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,16 +9,30 @@ import Section7 from "./homepageComponents/section7.js";
 import Section8 from "./homepageComponents/section8.js";
 import Section9 from "./homepageComponents/section9.js";
 import Footer from "./homepageComponents/footer.js";
+import { useEffect } from "react";
+import dynamic from "next/dynamic";
+import '@ryaneewx/react-chat-widget/lib/styles.css';
 
-/*
+// Dynamically import the Chat Widget component
+const ChatWidget = dynamic(() => import('@ryaneewx/react-chat-widget').then((mod) => mod.Widget), { ssr: false });
 
-        
-        
+export default function Home() {
+  useEffect(() => {
+    // Dynamically import addResponseMessage function
+    import('@ryaneewx/react-chat-widget').then(({ addResponseMessage }) => {
+      addResponseMessage("Welcome to our chat!");
+    });
+  }, []);
 
-*/
+  // Custom handler for new user messages
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Handle the new message as needed (e.g., send to a server or API)
+    import('@ryaneewx/react-chat-widget').then(({ addResponseMessage }) => {
+      addResponseMessage("Welcome to our chat!");
+    });
+  };
 
-export default function Home()
-{
   return (
     <>
       <Menu />
@@ -27,11 +41,11 @@ export default function Home()
         {/*
         <Section2 />
         <Section3 />
-  */}
+        */}
         <Section4 />
         {/*
         <Section7 />
-         
+
         <Section5 />
         <Section6 />
         <Section8 />
@@ -39,6 +53,11 @@ export default function Home()
         */}
         <Footer />
       </main>
+      <ChatWidget
+        title="Context Pilot"
+        subtitle="What a wonderful day!"
+        handleNewUserMessage={handleNewUserMessage}
+      />
     </>
-  )
+  );
 }
