ab1aa7783491b0f1df6d1adc46518e2d1b7c0682
Pilot Context <pilot.context@cryptitalk.com>
2024-08-05 16:40:44 +0000

    add google tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -136,8 +136,8 @@ export default function Home() {
     <>
       <Menu />
       <main>
-        <HomeSection />
         {/*
+        <HomeSection />
         <Section2 />
         <Section3 />
         */}
